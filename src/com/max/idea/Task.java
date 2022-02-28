965519e5809e809cb9c400ac5b15ae2582044631
NikitinaL <LNikitina@appline.ru>
2022-02-28 14:48:42 +0300

    Initial commit

---
+++ src/com/max/idea/Task.java
@@ -0,0 +1,11 @@
+package com.max.idea;
+
+public class Task {
+    public static void main(String[] args) {
+        double a = 10.24;
+        byte b = 127;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
