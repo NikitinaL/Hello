f0b6982a84551e5beae4277d92b32630396ac196
NikitinaL <LNikitina@appline.ru>
2022-03-15 09:38:38 +0300

    Initial commit

--- src/com/max/idea/mass.java
+++ src/com/max/idea/mass.java
@@ -7,9 +7,10 @@ public class mass {
         int [] myArraw = new int[5];
         int [] nums ={9, 8, 7, 6, 5};
         int last= nums [nums.length-1];
-        System.out.println(last);
-        System.out.println(nums[0]);
-        int sum = Arrays.stream(myArraw).sum();
-        System.out.println(sum);
+        int first =  nums[0];
+        nums[0]=last;
+        nums [nums.length-1]=first;
+        System.out.println(nums[0]*nums[2]);
+
     }
 }
